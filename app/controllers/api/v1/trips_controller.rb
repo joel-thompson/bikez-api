6d5b72058b1b1c8670f1a19d416ca82bdb618e81
Joel Thompson <joel.thompson@intercom.io>
2020-05-20 13:22:32 -0700

    first controller

---
+++ app/controllers/api/v1/trips_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::TripsController < ActionController::API
+  def index
+    trips = [
+      {trip_name: "whistler", id: 1},
+      {trip_name: "northstar", id: 2}
+    ]
+    
+    render json: { results: trips }.to_json, status: :ok
+  end
+end

357907a76399cd3d56edc9be4331915b7515d39b
Joel Thompson <joel.thompson@intercom.io>
2020-05-22 15:25:45 -0700

    cookie stuff

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_bikez", domain: "https://api.bikenumbers.dev"
+  Rails.application.config.session_store :cookie_store, key: "_bikez", domain: "https://bikenumbers.dev"
 else
   Rails.application.config.session_store :cookie_store, key: "_bikez"
 end

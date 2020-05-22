0d21279c30411b19ad66cbafdc9d95067ef49786
Joel Thompson <joel.thompson@intercom.io>
2020-05-22 07:38:18 -0700

    hopefully fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_bikez", domain: "https://bikez.herokuapp.com/"
+  Rails.application.config.session_store :cookie_store, key: "_bikez", domain: "https://bikez.herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_bikez"
 end

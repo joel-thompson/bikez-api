049360c55183f4c60edb419b0d548298aee8c602
Joel Thompson <joel.thompson@intercom.io>
2020-05-22 07:50:18 -0700

    fix cors again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["http://localhost:3000", "https://bikez-api.herokuapp.com"]
+    origins ["http://localhost:3000", "https://bikez.herokuapp.com"]
 
 
     resource '*',

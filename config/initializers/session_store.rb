40020fe05739a5069bf11a2c41e2092097f811f0
Joel Thompson <joel.thompson@intercom.io>
2020-05-22 15:40:25 -0700

    last try

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_bikez", domain: "https://api.bikenumbers.dev", tld_length: 2
+  Rails.application.config.session_store :cookie_store, key: "_bikez", domain: :all, tld_length: 2
 else
   Rails.application.config.session_store :cookie_store, key: "_bikez"
 end

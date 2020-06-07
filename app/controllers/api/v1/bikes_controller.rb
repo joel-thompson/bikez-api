31ec8c6a7e6b8fbc29f9d390cee915d4bcf45c6b
Joel Thompson <joel.thompson@intercom.io>
2020-06-07 12:48:28 -0700

    adding more actions

--- app/controllers/api/v1/bikes_controller.rb
+++ app/controllers/api/v1/bikes_controller.rb
@@ -1,21 +1,36 @@
 class Api::V1::BikesController < AuthenticatedApplicationController
-
   def index
     render json: @current_user.bikes, status: 200
   end
 
-  def create
-    
-  end
-
   def show
-    render json: @current_user.bikes.find_by(id: params[:id]), status: 200
+    bike = @current_user.bikes.find_by(id: params[:id])
+
+    if bike
+      render json: bike, status: 200
+    else
+      render json: { errors: ["bike not found"] }, status: 404
+    end
   end
 
-  def destroy
-    
+  def create
+    bike = Bike.create(
+      user: @current_user,
+      name: params[:name],
+    )
+
+    if bike.persisted?
+      render json: bike, status: 200
+    else
+      render json: { errors: bike.errors.full_messages }, status: 400
+    end
   end
 
   def update
+    bike = @current_user.bikes.find_by(id: params[:id])
   end
+
+  def destroy
+  end
+
 end
--- spec/controllers/api/v1/bikes_controller_spec.rb
+++ spec/controllers/api/v1/bikes_controller_spec.rb
@@ -43,16 +43,96 @@ RSpec.describe Api::V1::BikesController, type: :controller do
   end
 
   context "#show" do
-     let!(:bike) { create(:bike, user: user, name: "patrol") }
+    let!(:bike) { create(:bike, user: user, name: "patrol") }
 
     it "returns the bike json" do
       # expected_json = { id: bike.id, name: "patrol" }.to_json
       expected_json = BikeSerializer.new(bike).to_json
 
-
       get :show, params: {id: bike.id}, session: valid_session
       expect(response).to be_successful
       expect(response.body).to eq expected_json
+    end
+
+    it "returns 404 if the bike cant be found" do
+      get :show, params: {id: 500}, session: valid_session
+      expect(response).to_not be_successful
+      expect(response.status).to eq 404
+      expect(JSON.parse(response.body)["errors"]).to include "bike not found"
+    end
+
+    it "returns 404 if the bike belongs to another user" do
+      bike = create(:bike)
+      get :show, params: {id: bike.id}, session: valid_session
+      expect(response).to_not be_successful
+      expect(response.status).to eq 404
+      expect(JSON.parse(response.body)["errors"]).to include "bike not found"
+    end
+  end
+
+  context "#create" do
+    it "creates a bike" do
+      expect{
+        post :create, params: { name: "new bike" }, session: valid_session
+      }.to change{Bike.count}.from(0).to(1)
+
+      expect(response).to be_successful
+    end
+
+    it "returns the json" do
+      new_bike = create(:bike, user: user, name: "new bike")
+
+      expect(Bike).to receive(:create).with(
+        user: user,
+        name: new_bike.name,
+      ).and_return(new_bike)
+
+      post :create, params: { name: new_bike.name }, session: valid_session
+
+      expect(response).to be_successful
+      expect(response.body).to eq(BikeSerializer.new(new_bike).to_json)
+    end
+
+    it "returns errors if unable to create" do
+      post :create, params: { name: nil }, session: valid_session
+      expect(response).to_not be_successful
+      expect(response.status).to eq 400
+      expect(JSON.parse(response.body)["errors"]).to include "Name can't be blank"
+    end
+  end
+
+  context "#update" do
+
+    let(:bike) { create(:bike, user: user, name: "update me") }
+
+    it "updates the bike and returns the correct json" do
+      put :update, params: { id: bike.id, name: "updated name" }, session: valid_session
+      expect(response).to be_successful
+      bike.reload
+      expect(bike.name).to eq "updated name"
+      expect(response.body).to eq(BikeSerializer.new(bike).to_json)
+    end
+
+    it "returns errors if unable to update" do
+      put :update, params: { id: bike.id, name: nil }, session: valid_session
+      expect(response).to_not be_successful
+      expect(response.status).to eq 400
+      expect(JSON.parse(response.body)["errors"]).to include "Name can't be blank"
+    end
+
+    it "returns 404 if the bike cant be found" do
+      put :update, params: { id: 500, name: "updated name" }, session: valid_session
+      expect(response).to_not be_successful
+      expect(response.status).to eq 404
+      expect(JSON.parse(response.body)["errors"]).to include "bike not found"
+    end
+
+    it "returns 404 if the bike belongs to another user" do
+      bike = create(:bike)
+      put :update, params: { id: bike.id, name: "updated name" }, session: valid_session
+      expect(response).to_not be_successful
+      expect(response.status).to eq 404
+      expect(JSON.parse(response.body)["errors"]).to include "bike not found"
     end
   end
 end
